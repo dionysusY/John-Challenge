9329e09545ca8430d8d1989d636eef2cd3d3a509
Yinzhao L <13865176837@163.com>
2023-08-10 11:33:05 +0000

    getTransactionByCategory接口

--- src/main/java/com/boot/challenge/controller/TransactionController.java
+++ src/main/java/com/boot/challenge/controller/TransactionController.java
@@ -4,10 +4,7 @@ import com.boot.challenge.entity.Customer;
 import com.boot.challenge.entity.Transactions;
 import com.boot.challenge.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
