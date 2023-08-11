3600c60f9b551b5536ab010abd043ad5a29d0c56
Xiaomin L <1752591115@qq.com>
2023-08-11 08:29:02 +0000

    fix the bug of find by gender

--- src/main/java/com/boot/challenge/service/TransactionService.java
+++ src/main/java/com/boot/challenge/service/TransactionService.java
@@ -44,6 +44,12 @@ public class TransactionService {
         return transactionDAO.findTransactionsByMerchant(merchant);
     }
     public PageResponse getTransactionsGenderByPagination(String gender, int pageno, int size) {
+        if (pageno < 0) {
+            pageno = 0;
+        }
+        if(size <=0) {
+            size = 10;
+        }
         List<Transactions> transactionsList = getTransactionsByGender(gender);
         Pageable pageable = PageRequest.of(pageno, size);
         int start = pageable.getPageNumber() * pageable.getPageSize();
