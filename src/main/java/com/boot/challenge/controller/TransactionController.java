618e1d0a62984e28f77e0db54fc9cc5103f46904
Zhang Yushi <yszhang6161@outlook.com>
2023-08-11 09:06:54 +0000

    realize Pagination of finding transaction by merchant

--- src/main/java/com/boot/challenge/controller/TransactionController.java
+++ src/main/java/com/boot/challenge/controller/TransactionController.java
@@ -46,9 +46,11 @@ public class TransactionController {
         return transactionService.getTransactionByCategoryPagination(category,pageNo,size);
     }
     @GetMapping("/merchant/{merchant}")
-    public List<Transactions> getTransactionsByMerchant(@PathVariable String merchant){
+    public PageData<Transactions> getTransactionsByMerchant(@PathVariable String merchant,
+                                                        @RequestParam(defaultValue = "0")int pageNo,
+                                                        @RequestParam(defaultValue = "10") int size){
         System.out.println(merchant);
-        return transactionService.getTsansactionsByMerchant(merchant);
+        return transactionService.getTransactionsByMerchantPagination(merchant, pageNo, size);
     }
     @GetMapping("/amount")
     public List<Transactions> getTransactionsByAmount(@RequestParam(defaultValue = "1")int sort,@RequestParam(defaultValue = "0")int pageNo,
