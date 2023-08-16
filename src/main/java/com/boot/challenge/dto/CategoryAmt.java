1525f46113c7ea10321f9652f204eff9f11efecb
Yinzhao L <13865176837@163.com>
2023-08-16 05:33:53 +0000

    pom

---
+++ src/main/java/com/boot/challenge/dto/CategoryAmt.java
@@ -0,0 +1,22 @@
+package com.boot.challenge.dto;
+
+public class CategoryAmt {
+    private String category;
+    private double total_Amt;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getTotal_Amt() {
+        return total_Amt;
+    }
+
+    public void setTotal_Amt(double total_Amt) {
+        this.total_Amt = total_Amt;
+    }
+}
