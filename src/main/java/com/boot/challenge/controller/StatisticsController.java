9e809557f1c4058411a296b8b52762d9fa215751
Wenxuan Y <970163114@qq.com>
2023-08-17 07:50:11 +0000

    fix statistics bug

--- src/main/java/com/boot/challenge/controller/StatisticsController.java
+++ src/main/java/com/boot/challenge/controller/StatisticsController.java
@@ -9,8 +9,10 @@ import com.boot.challenge.dto.CategoryAmt;
 import com.boot.challenge.dto.StateAmt;
 import com.boot.challenge.dto.*;
 import com.boot.challenge.service.TransactionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,43 +20,39 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/statistics")
 public class StatisticsController {
 
     @Autowired
     private TransactionService transactionService;
 
-    @LogAnnotation(module = "Statistics", operator = "getAmtByMerchant")
     @GetMapping("/amtbymerchant")
     public List<MerchantAmt> getAmtByMerchant(){
         return transactionService.getAmtByMerchant();
     }
 
-    @LogAnnotation(module = "Statistics", operator = "getAmtByState")
     @GetMapping("/amtbystate")
     public List<StateAmt> getAmtByState() {
         return transactionService.getAmtByState();
     }
 
-    @LogAnnotation(module = "Statistics", operator = "getAmtByCity")
     @GetMapping("/amtbycity")
     public List<CityAmt> getAmtByCity(){
         return transactionService.getAmtByCity();
     }
 
 
-    @LogAnnotation(module = "Statistics", operator = "getAmtByGender")
     @GetMapping("/amtbygender")
     public List<GenderAmt> getAmtByGender(){
         return transactionService.getAmtByGender();
     }
 
-    @LogAnnotation(module = "Statistics", operator = "getAmtByPopulation")
     @GetMapping("/amtbypopulation")
     public List<PopulationAmt> getAmtByPopulation(){
         return transactionService.getAmtByPopulation();
     }
-    @LogAnnotation(module = "Statistics", operator = "getAmtByCategory")
+
     @GetMapping("/amtByCategory")
     public List<CategoryAmt> getAmtByCategory(){
         return transactionService.getAmtByCategory();
